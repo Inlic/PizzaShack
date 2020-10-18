cdec0cfee7826565fbc1ae68bfa6329b4eee1b6d
Ben W <bwiedeback@gmail.com>
2020-10-17 20:42:20 -0600

    initialized project

---
+++ Models/Pizza.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PizzaShack.Models
+{
+  public enum PizzaSizes
+  {
+    Personal = 1,  // starts at 0 unless given a value
+    Medium,
+    Large,
+    Family
+  }
+  public class Pizza
+  {
+    private decimal _BasePrice = 6;
+    public List<Topping> Toppings { get; set; }
+    public PizzaSizes Size { get; set; }
+
+    public decimal Price
+    {
+      get
+      {
+        return _BasePrice + (decimal)Size + Toppings.Sum(topping => topping.Price);
+      }
+    }
+  }
+}
---
+++ Models/Topping.cs
@@ -0,0 +1,8 @@
+namespace PizzaShack.Models
+{
+  public class Topping
+  {
+    public string Name { get; set; }
+    public decimal Price { get; set; } // whenever we're looking at real world pricing we use this
+  }
+}
